b05cc01327a030ba9c2a79188033b94264c0bf70
toptalentdevteam <david.bayne.yo@gmail.com>
2023-04-21 11:22:22 +0400

    16:20 4/21

--- new/resources/views/layouts/navbars/auth/sidenav.blade.php
+++ new/resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -3,14 +3,13 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="{{ route('home') }}"
-            target="_blank">
+        <a class="navbar-brand m-0" target="_blank">
             <img src="./img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">Admin Actions</span>
         </a>
     </div>
     <hr class="horizontal dark mt-0">
-    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+    <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             {{-- <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() == 'home' ? 'active' : '' }}" href="{{ route('home') }}">
